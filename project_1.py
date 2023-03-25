cfa1b81d59c9c5be4ac043320de21b51af528cf0
MatejFrolik <matejfrolik1@seznam.cz>
2023-03-25 12:52:36 +0100

    Výpočet slov s velkým písmenem

--- project_1.py
+++ project_1.py
@@ -97,6 +97,13 @@ for word in selected_text.split():
     )
 words_count = len(raw_words)
 
+s = selected_text.strip()
+count = 1
+for i in s:
+    if i.isupper():
+         count = count + 1
+print(count)
+
 
 
 
